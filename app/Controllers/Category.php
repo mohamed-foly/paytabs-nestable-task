e3f62871d2f4126c6fec48873551050880e98726
Mohamed Foly <mohamed.foly@twasal.co>
2020-06-08 01:33:25 +0200

    [category][controllers] get categories

--- app/Controllers/Category.php
+++ app/Controllers/Category.php
@@ -1,9 +1,12 @@
 <?php namespace App\Controllers;
 
 use App\Models\CategoryModel;
+use CodeIgniter\API\ResponseTrait;
 
 class Category extends BaseController
 {
+	use ResponseTrait;
+
 	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
 	{
 		parent::initController($request, $response, $logger);
@@ -14,6 +17,16 @@ class Category extends BaseController
 	public function index()
 	{
 		return view('category');
+	}
+
+	public function getAjaxCategories()
+	{
+		if($this->request->isAJAX()) {
+			header('Content-Type: application/json');
+
+			$categories = $this->category->getCategory();
+			return $this->respond($categories,200);
+		}
 	}
 
 }
