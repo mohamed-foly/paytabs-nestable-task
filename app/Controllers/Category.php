59bd7c5f66ee5b2f729ba5aab5bcfd49091f5b26
Mohamed Foly <mohamed.foly@twasal.co>
2020-06-08 00:51:35 +0200

    adding category controller

---
+++ app/Controllers/Category.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+use App\Models\CategoryModel;
+
+class Category extends BaseController
+{
+	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+	{
+		parent::initController($request, $response, $logger);
+
+		$this->category =  new CategoryModel();
+	}
+
+}
