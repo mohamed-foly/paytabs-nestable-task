3142412fc953c51d146df42d6af3d05f1e768cbf
Mohamed Foly <mohamed.foly@twasal.co>
2020-06-08 03:03:47 +0200

    [category][views] add display view

---
+++ app/Views/category.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  	<head>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="description" content="">
+	    <meta name="url" content="<?= base_url() ?>">
+
+	    <title>Codeigniter Nestable Menu</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom styles for this template -->
+	    <link href="https://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/sticky-footer-navbar.css" rel="stylesheet">
+  	</head>
+
+	<body>
+
+	    <header>
+	      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+	        <a class="navbar-brand" href="#"><i class="fas fa-fire"></i> Codeigniter Nestable</a>
+	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+	          	<span class="navbar-toggler-icon"></span>
+	        </button>
+	      </nav>
+	    </header>
+
+	    <main role="main" class="container">
+	    	<div class="nestable">
+	    	</div>
+		</main>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	    <script src="<?= base_url('assets/js/app.js') ?>"></script>
+  	</body>
+</html>
